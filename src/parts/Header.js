58146a1fd7442fffe06241d2f3e30a342e91dd17
HarrySetiawan25 <harrysetiawan2512@gmail.com>
2021-03-01 11:15:49 +0800

    Add react-reveal

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import Button from "elements/Button";
 import BrandIcon from "parts/iconText";
 
@@ -8,54 +9,69 @@ export default function Header(props) {
    };
 
    return (
-      <header className="spacing-sm">
-         <div className="container">
-            <nav className="navbar navbar-expand-lg navbar-light">
-               <BrandIcon />
-               <button
-                  class="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarNavigation"
-                  aria-controls="navbarNavigation"
-                  aria-label="Toggle navigation"
-               >
-                  <span class="navbar-toggler-icon"></span>
-               </button>
-               <div className="collapse navbar-collapse" id="navbarNavigation">
-                  <ul className="navbar-nav ml-auto">
-                     <li className={`nav-item${getNavLinkClass("/")}`}>
-                        <Button className="nav-link" type="link" href="/">
-                           Home
-                        </Button>
-                     </li>
-                     <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                        <Button
-                           className="nav-link"
-                           type="link"
-                           href="/browse-by"
+      <Fade>
+         <header className="spacing-sm">
+            <div className="container">
+               <nav className="navbar navbar-expand-lg navbar-light">
+                  <BrandIcon />
+                  <button
+                     class="navbar-toggler"
+                     type="button"
+                     data-toggle="collapse"
+                     data-target="#navbarNavigation"
+                     aria-controls="navbarNavigation"
+                     aria-label="Toggle navigation"
+                  >
+                     <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                     className="collapse navbar-collapse"
+                     id="navbarNavigation"
+                  >
+                     <ul className="navbar-nav ml-auto">
+                        <li className={`nav-item${getNavLinkClass("/")}`}>
+                           <Button className="nav-link" type="link" href="/">
+                              Home
+                           </Button>
+                        </li>
+                        <li
+                           className={`nav-item${getNavLinkClass(
+                              "/browse-by"
+                           )}`}
                         >
-                           Browse By
-                        </Button>
-                     </li>
-                     <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                        <Button
-                           className="nav-link"
-                           type="link"
-                           href="/stories"
+                           <Button
+                              className="nav-link"
+                              type="link"
+                              href="/browse-by"
+                           >
+                              Browse By
+                           </Button>
+                        </li>
+                        <li
+                           className={`nav-item${getNavLinkClass("/stories")}`}
                         >
-                           Stories
-                        </Button>
-                     </li>
-                     <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                        <Button className="nav-link" type="link" href="/agents">
-                           Agents
-                        </Button>
-                     </li>
-                  </ul>
-               </div>
-            </nav>
-         </div>
-      </header>
+                           <Button
+                              className="nav-link"
+                              type="link"
+                              href="/stories"
+                           >
+                              Stories
+                           </Button>
+                        </li>
+                        <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                           <Button
+                              className="nav-link"
+                              type="link"
+                              href="/agents"
+                           >
+                              Agents
+                           </Button>
+                        </li>
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+         </header>
+      </Fade>
    );
 }
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import ImgHero from "assets/images/img-hero.jpg";
 import ImgHeroFrame from "assets/images/img-hero-frame.jpg";
 import IconCities from "assets/images/icons/ic_city.svg";
@@ -15,95 +16,97 @@ export default function Hero(props) {
       });
    }
    return (
-      <section className="container pt-4">
-         <div className="row align-items-center">
-            <div
-               className="col-md-9 col-lg-6 pr-5 mx-auto"
-               style={{ width: 530 }}
-            >
-               <h1 className="font-weight-bold line-height-1 mb-3">
-                  Forget Busy Work, <br />
-                  Start Next Vacation
-               </h1>
-               <p
-                  className="mb-4 font-weight-light text-gray-500 w-75"
-                  style={{ lineHeight: "170%" }}
+      <Fade bottom>
+         <section className="container pt-4">
+            <div className="row align-items-center">
+               <div
+                  className="col-md-9 col-lg-6 pr-5 mx-auto"
+                  style={{ width: 530 }}
                >
-                  We provide what you need to enjoy your holiday with family.
-                  Time to make more memorable moments.
-               </p>
-               <Button
-                  className="btn px-5"
-                  hasShadow
-                  isPrimary
-                  onClick={showMostPicked}
-               >
-                  Show Me Now
-               </Button>
-               <div className="row" style={{ marginTop: 80 }}>
-                  <div className="col-4">
-                     <img
-                        width="36"
-                        height="36"
-                        src={IconTravelers}
-                        alt={`${props.data.travelers} travelers`}
-                     />
-                     <h6 className="mt-3">
-                        {numberFormat(props.data.travelers)}{" "}
-                        <span className="text-gray-500 font-weight-light">
-                           travelers
-                        </span>
-                     </h6>
+                  <h1 className="font-weight-bold line-height-1 mb-3">
+                     Forget Busy Work, <br />
+                     Start Next Vacation
+                  </h1>
+                  <p
+                     className="mb-4 font-weight-light text-gray-500 w-75"
+                     style={{ lineHeight: "170%" }}
+                  >
+                     We provide what you need to enjoy your holiday with family.
+                     Time to make more memorable moments.
+                  </p>
+                  <Button
+                     className="btn px-5"
+                     hasShadow
+                     isPrimary
+                     onClick={showMostPicked}
+                  >
+                     Show Me Now
+                  </Button>
+                  <div className="row" style={{ marginTop: 80 }}>
+                     <div className="col-4">
+                        <img
+                           width="36"
+                           height="36"
+                           src={IconTravelers}
+                           alt={`${props.data.travelers} travelers`}
+                        />
+                        <h6 className="mt-3">
+                           {numberFormat(props.data.travelers)}{" "}
+                           <span className="text-gray-500 font-weight-light">
+                              travelers
+                           </span>
+                        </h6>
+                     </div>
+                     <div className="col-4">
+                        <img
+                           width="36"
+                           height="36"
+                           src={IconTreasures}
+                           alt={`${props.data.treasures} treasures`}
+                        />
+                        <h6 className="mt-3">
+                           {numberFormat(props.data.treasures)}{" "}
+                           <span className="text-gray-500 font-weight-light">
+                              treasures
+                           </span>
+                        </h6>
+                     </div>
+                     <div className="col-4">
+                        <img
+                           width="36"
+                           height="36"
+                           src={IconCities}
+                           alt={`${props.data.cities} cities`}
+                        />
+                        <h6 className="mt-3">
+                           {numberFormat(props.data.cities)}{" "}
+                           <span className="text-gray-500 font-weight-light">
+                              cities
+                           </span>
+                        </h6>
+                     </div>
                   </div>
-                  <div className="col-4">
+               </div>
+               <div className="col-md-9 col-lg-6 pl-3 mx-auto">
+                  <div style={{ width: 520, height: 410 }}>
                      <img
-                        width="36"
-                        height="36"
-                        src={IconTreasures}
-                        alt={`${props.data.treasures} treasures`}
+                        height="450"
+                        src={ImgHero}
+                        alt="Tabby Town"
+                        className="img-fluid position-absolute"
+                        style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
                      />
-                     <h6 className="mt-3">
-                        {numberFormat(props.data.treasures)}{" "}
-                        <span className="text-gray-500 font-weight-light">
-                           treasures
-                        </span>
-                     </h6>
-                  </div>
-                  <div className="col-4">
                      <img
-                        width="36"
-                        height="36"
-                        src={IconCities}
-                        alt={`${props.data.cities} cities`}
+                        height="450"
+                        src={ImgHeroFrame}
+                        alt="Tabby Town Frame"
+                        className="img-fluid position-absolute"
+                        style={{ margin: "0 -15px -15px 0" }}
                      />
-                     <h6 className="mt-3">
-                        {numberFormat(props.data.cities)}{" "}
-                        <span className="text-gray-500 font-weight-light">
-                           cities
-                        </span>
-                     </h6>
                   </div>
                </div>
             </div>
-            <div className="col-md-9 col-lg-6 pl-3 mx-auto">
-               <div style={{ width: 520, height: 410 }}>
-                  <img
-                     height="450"
-                     src={ImgHero}
-                     alt="Tabby Town"
-                     className="img-fluid position-absolute"
-                     style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-                  />
-                  <img
-                     height="450"
-                     src={ImgHeroFrame}
-                     alt="Tabby Town Frame"
-                     className="img-fluid position-absolute"
-                     style={{ margin: "0 -15px -15px 0" }}
-                  />
-               </div>
-            </div>
-         </div>
-      </section>
+         </section>
+      </Fade>
    );
 }

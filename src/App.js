0324d6043489ee11547376be07af3a5c35b78af5
HarrySetiawan25 <harrysetiawan2512@gmail.com>
2021-03-01 23:39:02 +0800

    Create component inputNumber

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LandingPage from "pages/LandingPage";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Example from "pages/Example";
 
 import "assets/scss/style.scss";
 function App() {
